fb4e4578f01893ff043120063778cd811a8c9cb2
GiSokNam <ngs990@naver.com>
2024-03-18 18:55:18 +0900

    feat: first commit  - README 작성

---
+++ src/main/java/com/imitation/amity/AmityApplication.java
@@ -0,0 +1,13 @@
+package com.imitation.amity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmityApplication.class, args);
+	}
+
+}
